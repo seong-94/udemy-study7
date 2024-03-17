fb10c0b00292a089f9e14f0e06379eb821f0c1c2
seong-94 <togussla@naver.com>
2024-03-17 23:14:54 +0900

    강의251

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,51 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enterValue, setEnterValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleInputChange(identifier, value) {
+    setEnterValue((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
+  }
+  // function handleEmailChange() {
+  //   setEnterValue(event.target.value);
+  // }
+  // function handlePasswordChange() {
+  //   setEnterValue(event.target.value);
+  // }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={() => handleInputChange("email", event.target.value)}
+            value={enterValue.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={() => handleInputChange("password", event.target.value)}
+            value={enterValue.password}
+          />
         </div>
       </div>
 
